820037648c9b7c077e2ea6baf952c5928d2a3721
sunilpaulmathew <sunil.kde@gmail.com>
2017-10-25 16:25:21 +0900

    drivers/cpufreq: adding smartmax and interactive_pro cpufreq govs
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -483,6 +483,12 @@ extern struct cpufreq_governor cpufreq_gov_interactive_pro;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE_PRO)
+extern struct cpufreq_governor cpufreq_gov_interactive_pro;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive_pro)
 #endif
 
 /*********************************************************************
